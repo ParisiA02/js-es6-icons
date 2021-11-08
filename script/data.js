897d557b6b5dc428421320344b4145cf5f26cb62
Andrea Parisi <andreaparisi4502@gmail.com>
2021-11-08 16:26:58 +0100

    added filter selector

--- script/data.js
+++ script/data.js
@@ -1,3 +1,10 @@
+/*
+	1. creo una funzione che mi permeta di stampare per ogni elemento dell'array l'icona all'interno della pagina
+	2. aggiungo un listener che mi permetta di gestire il selector dell'html
+	3. gestisco il filtro all'interno della pagina con delle condizioni 
+		3.1 per ogni filtro stampo le icone appartenenti al gruppo di icone selezionato
+*/
+
 const iconArray = [
 	{
 		name: 'cat',
@@ -113,16 +120,47 @@ const iconArray = [
 	}
 ];
 
-// iconArray.forEach(element => {
-	
-// });
-
 let container = document.querySelector(".container");
 
-iconArray.forEach(element => {
+stampaIcone(iconArray);
 
-	container.innerHTML += `<div class="box">
-								<i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color};"></i>
-								<div class="text">${element.name.toUpperCase()}</div>
-							</div>`;
+document.addEventListener("change", function(){
+	if (selector.value == "vegetable"){
+		const vegetableArray = iconArray.filter((element)=>{
+			return element.type === "vegetable";
+		});
+
+		container.innerHTML="";
+		stampaIcone(vegetableArray);
+
+	}else if(selector.value == "user"){
+		const userArray = iconArray.filter((element)=>{
+			return element.type === "user"
+		});
+
+		container.innerHTML="";
+		stampaIcone(userArray);
+
+	}else if(selector.value == "animal"){
+		const animalArray = iconArray.filter((element)=>{
+			return element.type === "animal";
+		});
+
+		container.innerHTML="";
+		stampaIcone(animalArray);
+
+	}else if (selector.value == "all"){
+		container.innerHTML="";
+		stampaIcone(iconArray);
+	}
 });
+
+function stampaIcone(element){
+	element.forEach(element => {
+		let elemento = `<div class="box">
+							<i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color};"></i>
+							<div class="text">${element.name.toUpperCase()}</div>
+						</div>`;
+		container.innerHTML += elemento;
+	});
+}
